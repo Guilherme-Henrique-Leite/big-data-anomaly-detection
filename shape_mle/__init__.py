dfcf883f2ff93b37b1352734f0b670100dac5c26
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2025-02-27 02:33:21 +0000

    feat: Create logging to  monitoring the application

--- shape_mle/__init__.py
+++ shape_mle/__init__.py
@@ -1,1 +1,5 @@
+"""
+Shape MLE Challenge Solution
+"""
 
+__version__ = "0.1.0"
---
+++ shape_mle/infrastructure/logging.py
@@ -0,0 +1,40 @@
+"""
+Logging module for application.
+"""
+
+import logging
+import datetime
+
+from shape_mle.config import LOG_FILE_PATH
+
+
+def setup_logger() -> logging.Logger:
+    """
+    Configures and returns a basic logger
+    
+    :return: Configured logger
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(levelname)s: %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
+    
+    logger = logging.getLogger('shape_mle')
+    
+    return logger
+
+
+logger = setup_logger()
+
+
+def log_failure(exception: Exception) -> None:
+    """
+    Logs a failure with date/time in the log file
+    
+    :exception: The exception to be logged
+    """
+    logger.error(f"Failure: {str(exception)}")
+    
+    with open(LOG_FILE_PATH, 'a') as failure_log:
+        failure_log.write(f'{datetime.datetime.now()} - Failure: {str(exception)}\n')
